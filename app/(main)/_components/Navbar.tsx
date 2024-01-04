2463d99c609ac0d017b678f394b20a1e65ca14f5
Vlad Polevoi <vlad.polewoi1@gmail.com>
2024-01-04 15:08:07 +0200

    feat: :sparkles: Navbar

--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -5,6 +5,8 @@ import { useQuery } from 'convex/react'
 import { MenuIcon } from 'lucide-react'
 import { useParams } from 'next/navigation'
 import Title from './Title'
+import Banner from './Banner'
+import Menu from './Menu'
 
 interface NavbarProps {
   isCollapsed: boolean
@@ -17,8 +19,11 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 
   if (document === undefined) {
     return (
-      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4 ">
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between gap-x-4 ">
         <Title.Skeleton />
+        <div className="flex items-center gap-x-2">
+          <Menu.Skeleton />
+        </div>
       </nav>
     )
   }
@@ -33,8 +38,12 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
         {isCollapsed && <MenuIcon role="button" onClick={onResetWidth} className="h-6 w-6 text-muted-foreground" />}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
+      {document.isArchived && <Banner documentId={document._id} />}
     </>
   )
 }
